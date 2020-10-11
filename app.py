67bca4c5e8462c82d66aabc16271ac2eba552378
Namank123 <nk.er13.6@gmail.com>
2020-10-11 17:58:58 +0530

    Add a new route new feature

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask
 
 app=Flask(__name__)
 
-
-@app.route("/newfeature")
-def new_feature():
-    return "This route was added by Saksham Basandrai"
-
 @app.route("/")
 def home():
 	return "Hello Hacktoberfest!"
+
+@app.route("/newfeatures")
+def new_feature():
+	return "This route was added by Naman Kumar"
 
 if __name__=='__main__':
 	app.run(port=5500)
